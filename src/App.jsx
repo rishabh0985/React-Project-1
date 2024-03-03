1c3bb99d8e774dfc7823fcb97542e6327ec760ee
Rishabh kushwah <128125196+rishabh0985@users.noreply.github.com>
2024-03-03 18:56:04 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from '.hello.jsx'
+
+function App() {
+
+  return (
+    <div>hello world</div>
+  )
+}
+
+export default App
---
+++ src/hello.jsx
@@ -0,0 +1,9 @@
+function Rk (){
+    return(
+        <>
+        <h1>kya haal hain </h1>
+        <p>run run run </p>
+        </>
+    )
+}
+export default Rk
